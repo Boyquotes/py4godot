576615d71bfcafd3003f34a23e94349fabc26727
niklas <niklaszimmer2902@gmail.com>
2023-02-24 20:03:08 +0100

    Previous commit did not work. Should be fixed now

--- py4godot/pluginscript_api/utils/annotations.pyx
+++ py4godot/pluginscript_api/utils/annotations.pyx
@@ -19,40 +19,37 @@ class TransferObject():
         self.signals = signals
 
 def exec_class(source_string, class_name_):
-    global methods, gd_class, gd_tool_class, properties,signals, class_name, save_props, save_methods, save_signals
+    global methods, gd_class, gd_tool_class, properties,signals, class_name
     methods = []
     gd_class = None
     gd_tool_class = None
     properties = []
     signals = []
-    save_props = []
-    save_methods = []
-    save_signals = []
     class_name = class_name_.split("/")[-1].replace(".py", "")
     exec(source_string, locals())
-    return TransferObject(save_methods, gd_class, gd_tool_class, save_props, save_signals)
+    return TransferObject(methods, gd_class, gd_tool_class, properties, signals)
 
 
 def gdclass(cls):
-        global gd_class, methods, properties, signals, save_methods, save_props, save_signals
+        global gd_class, methods, properties, signals
         if cls.__name__ != class_name:
+            methods = []
+            properties = []
+            signals = []
             return cls
 
         if(gd_class == None):
             gd_class = cls
         else:
             raise Exception("More than one class was marked as gd_class or gd_tool_class in one file")
-        save_methods = methods
-        save_props = properties
-        save_signals = signals
-        methods = []
-        properties = []
-        signals = []
         return cls
 
 def gdtool(cls):
         global gd_tool_class
         if cls.__name__ != class_name:
+            methods = []
+            properties = []
+            signals = []
             return cls
 
         if(gd_tool_class == None):
